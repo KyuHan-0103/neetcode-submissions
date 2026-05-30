66d3f44db6e8b8a6c09c94f3b6b15fb9f76cb8b0
KyuHan-0103 <nathanaellee0@gmail.com>
2026-05-30 10:28:52 +0700

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode cur = l1;
+        int excess = 0;
+        while(cur != null && l2 != null){
+            int val = (cur.val + l2.val) % 10;
+            excess += (cur.val + l2.val)/10;
+            cur.val = val;
+            if(excess > 0){
+                if(cur.next != null){
+                    int tmp = cur.next.val;
+                    cur.next.val = (tmp + excess) % 10;
+                    excess = (tmp + excess)/10;
+                    System.out.print(excess + ", ");
+                } else {
+                    cur.next = new ListNode(excess);
+                    excess = 0;
+                }
+            }
+            l2 = l2.next;
+            cur = cur.next;
+        }
+
+        while(cur != null){
+            if(excess == 0) break;
+
+            if(cur.next != null){
+                int tmp = cur.next.val;
+                cur.next.val = (tmp + excess) % 10;
+                excess = (tmp + excess)/10;
+            } else {
+                cur.next = new ListNode(excess);
+                break;
+            }
+
+            cur = cur.next;
+        }
+
+        cur = l1;
+        while(l2 != null){
+            cur.next = l2;
+            if(excess == 0) break;
+            /*
+            int tmp = l2.val;
+            l2.val = (tmp + excess) % 10;
+            excess = (tmp + excess)/10;
+            */
+            l2 = l2.next;
+        }
+        return l1;
+    }
+}
