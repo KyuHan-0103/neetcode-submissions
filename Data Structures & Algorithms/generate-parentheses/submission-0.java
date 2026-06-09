123129d3ee5270495d4c859a1c2424868730c74b
KyuHan-0103 <nathanaellee0@gmail.com>
2026-06-09 12:08:16 +0700

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack(res, new StringBuilder(), 0, 0, n);
+        return res;
+    }
+
+    private void backtrack(List<String> res, StringBuilder cur, 
+                           int open, int close, int n) {
+        if (cur.length() == n * 2) {
+            res.add(cur.toString());
+            return;
+        }
+        if (open < n) {
+            cur.append('(');
+            backtrack(res, cur, open + 1, close, n);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(res, cur, open, close + 1, n);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
