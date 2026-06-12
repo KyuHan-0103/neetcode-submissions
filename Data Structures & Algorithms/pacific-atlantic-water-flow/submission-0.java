40aa8478e54019a69475b2e043b74dec2c020056
KyuHan-0103 <nathanaellee0@gmail.com>
2026-06-11 21:44:13 -0400

    Add: pacific-atlantic-water-flow - submission-0

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int ROW = heights.length, COL = heights[0].length;
+        boolean[][] pac = new boolean[ROW][COL];
+        boolean[][] atl = new boolean[ROW][COL];
+
+        //Initiate which cells can reach their respective oceans
+        for(int c = 0; c < COL; c++){
+            dfs(0, c, pac, heights);
+            dfs(ROW - 1, c, atl, heights);
+        }
+
+        for(int r = 0; r < ROW; r++){
+            dfs(r, 0, pac, heights);
+            dfs(r, COL - 1, atl, heights);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for(int r = 0; r < ROW; r++){
+            for(int c = 0; c < COL; c++){
+                if(pac[r][c] && atl[r][c]){
+                    res.add(Arrays.asList(r, c));
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(int r, int c, boolean[][] ocean, int[][]heights){
+        ocean[r][c] = true;
+        for(int[] dir : directions){
+            int nr = r + dir[0], nc = c + dir[1];
+            if(nr < 0 || nc < 0 || nr >= heights.length
+            || nc >= heights[0].length || ocean[nr][nc]) continue;
+            if(heights[r][c] <= heights[nr][nc]){
+                dfs(nr, nc, ocean, heights);
+            }
+        }
+    }
+}
