bb0d69c66dc1fb857ebaf8d6dec776c08192c9cc
KyuHan-0103 <nathanaellee0@gmail.com>
2026-06-19 15:58:58 -0400

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    Set<String> visit = new HashSet<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return dfs(s, wordDict);
+    }
+
+    private boolean dfs(String s, List<String> wordDict){
+        if(s == "") return true;
+        if(visit.contains(s)){
+            return false;
+        }
+
+        for(String word : wordDict){
+            int index = s.indexOf(word);
+            if(index != -1){
+                if(dfs(s.substring(0, index), wordDict) && 
+                dfs(s.substring(index + word.length()), wordDict)){
+                    return true;
+                }
+            }
+        }
+
+        visit.add(s);
+        return false;
+    }
+}
